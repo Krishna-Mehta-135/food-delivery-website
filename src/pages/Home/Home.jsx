1a689a385098a4ad0530071761ef004578b4997e
Krishna Mehta <krishnamehta135@protonmail.com>
2025-07-24 20:23:39 +0530

    Setup the project and added the header

---
+++ src/pages/Home/Home.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import Layout from '../../components/Layouts/Layout'
+
+const Home = () => {
+  return (
+    <div>
+      <Layout>
+        <div>home</div>
+      </Layout>
+    </div>
+  )
+}
+
+export default Home

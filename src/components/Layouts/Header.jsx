8d643d9e109e8140b2ac37cac1abe3529047bf42
Krishna Mehta <krishnamehta135@protonmail.com>
2025-07-27 12:59:33 +0530

    Improves header and home section styling
    Implements a sticky header that activates on scroll, enhancing user navigation.
    Updates the header's CSS for improved appearance and responsiveness.
    Removes an unnecessary "btn" class from the Home section's order button, streamlining its styling.

--- src/components/Layouts/Header.jsx
+++ src/components/Layouts/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import "../../styles/HeaderStyle.css";
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
@@ -7,9 +7,18 @@ import {Link} from "react-router-dom";
 import Logo from "../../assets/logo/logo.png";
 
 function Header() {
+    const [nav, setNav] = useState(false);
+    //Scroll Navbar
+    const changeValueOnScroll = () => {
+        const scrollValue = document?.documentElement?.scrollTop;
+        scrollValue > 100 ? setNav(true) : setNav(false);
+    };
+
+    window.addEventListener("scroll", changeValueOnScroll);
+
     return (
         <header>
-            <Navbar collapseOnSelect expand="lg">
+            <Navbar collapseOnSelect expand="lg" className={`${nav == true ? "sticky" : ""}`}>
                 <Container>
                     <Navbar.Brand as={Link} to="/" className="logo">
                         <img src={Logo} alt="Logo" className="img-fluid" />
